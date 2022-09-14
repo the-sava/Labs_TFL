16a584e5238fca57ea84e3d91702acdb314cd6e8
Alexey <therealsava@icloud.com>
2022-09-14 04:42:11 +0300

    Add parsing terms

--- Лабораторная работа 1/knutBendix.js
+++ Лабораторная работа 1/knutBendix.js
@@ -1,22 +1,60 @@
 const fs = require('fs')
 const path = require('path')
 
+class Term {
+
+    //{c: f, args: {c: g, args: x}} = {c:g, args: {c:h, args: x,x}}
+    constructor(term) {
+        let [leftSide, rightSide] = term.split('=')
+        this.full = term
+        this.leftSide = leftSide
+        this.rightSide = rightSide
+        this.leftSideStructure = {}
+        this.rightSideStructure = {}
+    }
+
+    parse(term, key, structure) {
+        if (key === term.length - 1) {
+            return 0
+        }
+        else if (term[key] === '(') {
+            structure.args = []
+            this.parse(term, key + 1, structure.args)
+        }
+        else if (term[key] === ')') {
+            this.parse(term, key + 1, structure)
+        }
+        else if (term[key] === ',') {
+            structure.push(term[key + 1])
+            this.parse(term, key + 2, structure)
+        }
+        else {
+            structure.name = term[key]
+            this.parse(term, key + 1, structure)
+        }
+    }
+
+}
+
 class TRS {
 
-    constructor(fileName) {
+    constructor(name, fileName) {
+        this.name = name
         this.fileName = fileName
         this.fileData = null
         this.fileLines = []
         this.regexpForType = /lexicographic|anti-lexicographic/
-        this.regexpForConstructors = /constructors=([a-z]\([0-2]\),)*([a-z]\([0-2]\))/
-        this.regexpForConstructor = /[a-z]\([0-2]\)/g
+        this.regexpForConstructors = /constructors=([a-z]\(([0-9])+\),)*([a-z]\(([0-9])+\))/
+        this.regexpForConstructor = /[a-z]\(([0-9])+\)/g
         this.regexpForVariables = /variables=([a-z],)*[a-z]/
         this.regexpForVariable = /[a-z]/g
         this.isCorrectSyntax = true
         this.variables = []
         this.constructors = []
+        this.terms = []
         this.parseFile(fileName)
         this.checkInputFile()
+        this.checkTRS()
     }
 
     parseFile(fileName) {
@@ -36,14 +74,16 @@ class TRS {
                     if (line.match(this.regexpForType)) {
                         if (line.match(this.regexpForType)[0] !== line) throw errorOfType
                     } else throw errorOfType
-
                     break
                 case 1: {
                     let errorOfСonstructors = new Error('Ошибка синтаксиса списка конструкторов строке: ' + line)
                     if (line.match(this.regexpForConstructors)) {
                         if (line.match(this.regexpForConstructors)[0] !== line) throw errorOfСonstructors
                     } else throw errorOfСonstructors
-                    this.constructors = line.replace('constructors=', '').match(this.regexpForConstructor).map((constructor)=>{})
+                    this.constructors = line.replace('constructors=', '').match(this.regexpForConstructor).map( (constructor) => {
+                        let [constructorName, n] = constructor.replace('(', ' ').replace(')', '').split(' ')
+                        return {constructor : constructorName, n: Number(n)}
+                    })
                     break
                 }
                 case 2: {
@@ -54,18 +94,43 @@ class TRS {
                     this.variables = line.replace('variables=', '').match(this.regexpForVariable)
                     break
                 }
+                default: {
+                    this.terms.push(new Term(line))
+                    break
+                }
             }
 
         })
     }
 
+    checkTRS() {
+        for (let constructorKey in this.constructors) {
+            if ((this.constructors[constructorKey].n !== 1) && (this.constructors[constructorKey].n !== 2)) throw Error(`Арность конструктора может быть 1 или 2 ( Арность конструктора ${this.constructors[constructorKey].constructor}(${this.constructors[constructorKey].n}) равна ${this.constructors[constructorKey].n} )`)
+        }
+        for (let variableKey in this.variables) {
+            for (let constructorKey in this.constructors) {
+                if (this.variables[variableKey] === this.constructors[constructorKey].constructor) throw Error(`Множество переменных и конструкторов не могут пересекаться ( Совпадают: ${this.variables[variableKey]} и ${this.constructors[constructorKey].constructor}(${this.constructors[constructorKey].n}) )`)
+            }
+        }
+        for (let termKey in this.terms) {
+            this.terms[termKey].parse(this.terms[termKey].leftSide, 0, this.terms[termKey].leftSideStructure)
+            this.terms[termKey].parse(this.terms[termKey].rightSide, 0, this.terms[termKey].rightSideStructure)
+        }
+    }
+
+    printParsingTree() {
+
+    }
+
     print() {
-        console.log(this.fileLines)
+        console.log(`Имя: ${this.name}\nКонструкторы: ${this.constructors.map((constructor) => {return constructor.constructor + '(' + constructor.n + ')'}).join(', ')}\nПеременные: ${this.variables.join(', ')}\nТермы:\n${this.terms.map((term) => {return term.term}).join('\n')}`)
     }
 }
 
-let TRS1 = new TRS('input.txt')
+let TRS1 = new TRS('TRS1', 'input.txt')
 TRS1.print()
 console.log(TRS1.isCorrectSyntax)
-console.log(TRS1.fileLines[1].match(TRS1.regexpForConstructor))
+
+console.log(TRS1.terms)
+
 
